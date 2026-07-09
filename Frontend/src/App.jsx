4dede5dc4a0d1bad7ffad4e2ef97898371fa9ebb
Acharya-Sonam <sonamacharya0@gmail.com>
2026-07-09 14:12:14 +0545

    added reuired css

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,26 @@
-import ChairGrid from "./components/ChairGrid";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Sidebar from "./components/layout/Sidebar";
+
+import Dashboard from "./components/pages/Dashboard";
+import Queue from "./components/pages/Queue";
+import Customers from "./components/pages/Customers";
 
 function App() {
     return (
-        <div>
-            <ChairGrid />
-        </div>
+        <BrowserRouter>
+            <div className="app-container">
+                <Sidebar />
+
+                <div className="main-content">
+                    <Routes>
+                        <Route path="/" element={<Dashboard />} />
+                        <Route path="/queue" element={<Queue />} />
+                        <Route path="/customers" element={<Customers />} />
+                    </Routes>
+                </div>
+            </div>
+        </BrowserRouter>
     );
 }
 
